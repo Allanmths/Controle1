c6f397280823b7e6cc9973dc0cb5c7515f6f501f
Allanmths <allanmths@gmail.com>
2025-07-14 12:43:23 -0300

    Corrigir importação firebase-config e adicionar build para produção

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'; // Importar o getStorage
-import { firebaseConfig } from '../../firebase-config';
+import { firebaseConfig } from '../firebase-config';
 
 // Inicialização mais segura para evitar re-inicialização em HMR (Hot Module Replacement)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
